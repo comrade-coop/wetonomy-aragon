2796094505a3eb0aafe56ea66070659ae6e1a699
Ivan-Asen Chakarov <vankata9907@gmail.com>
2018-06-09 15:08:32 +0300

    Described purpose of DummyApp

--- contracts/misc/DummyApp.sol
+++ contracts/misc/DummyApp.sol
@@ -2,23 +2,12 @@ pragma solidity 0.4.18;
 
 import "@aragon/os/contracts/apps/AragonApp.sol";
 
-contract DummyApp is AragonApp {
-    ////
-    ////               ....
-    ////           .,,,,..,,,,.
-    ////       ..,,.. ..     .,,,..
-    ////     .,,.  ..,:....,,..  .,,.
-    ////    ,:   ...,.    .,,..,.   :,
-    ////    .:. ,. ,           ,.. .:.
-    ////     ,:,.  ..        .,,., :,
-    ////      ,;.   ........,..,..:,
-    ////       ,:.       .. .....:,
-    ////        .:,           .::.
-    ////          .,,.      .,,.
-    ////            .,,,..,,,.
-    ////               ....
-    ////
-    ////  Build something beautiful.
+// This is a "Dummy" app which's only purpose to exist is because
+// Aragon's CLI still doesn't support running a Kit inside a project
+// which isn't considered to be a "valid" Aragon project.
+// It requires us to have an arrap.json file pointing to the contract
+// and a manifest.json file which describes the front-end structure.
+contract DummyApp is AragonApp {    
     function initialize(string _name) onlyInit {
         initialized();
     }
