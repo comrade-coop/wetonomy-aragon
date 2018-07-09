8e9d287605413287a3ed6873cf0defddcc187d9d
Ivan-Asen Chakarov <vankata9907@gmail.com>
2018-07-09 13:21:42 +0300

    Added kit usage instructions to README.md

--- README.md
+++ README.md
@@ -48,3 +48,13 @@ This repository contains the following apps:
 - Run 
 	- ```aragon run```
 
+## Kit Usage
+```sh
+npm run devchain # Starts a local devchain
+
+npm run bootstrap # Bootstraps dependencies of the seperate projects inside apps/
+
+npm run build:apps # Builds the Front-end of the apps
+npm run publish:apps # Publishes the seperate apps to the devchain and IPFS
+npm run start:kit # Finally, creates a new DAO, given the kit file in contracts/
+```
--- apps/members/app/src/components/MembersTable.js
+++ apps/members/app/src/components/MembersTable.js
@@ -15,11 +15,11 @@ const Header = (
 
 const members = [
   { name: 'John Smith', accountAddress: '0x969F8A3667987823B84C4F22A4CdfEA3Ae724E86', level: 'Senior', reputation: 324, payRate: '$24/hr' },
-  { name: 'John Smith', accountAddress: '0x969F8A3667987823B84C4F22A4CdfEA3Ae724E86', level: 'Senior', reputation: 324, payRate: '$24/hr' },
-  { name: 'John Smith', accountAddress: '0x969F8A3667987823B84C4F22A4CdfEA3Ae724E86', level: 'Senior', reputation: 324, payRate: '$24/hr' },
-  { name: 'John Smith', accountAddress: '0x969F8A3667987823B84C4F22A4CdfEA3Ae724E86', level: 'Senior', reputation: 324, payRate: '$24/hr' },
-  { name: 'John Smith', accountAddress: '0x969F8A3667987823B84C4F22A4CdfEA3Ae724E86', level: 'Senior', reputation: 324, payRate: '$24/hr' },
-  { name: 'John Smith', accountAddress: '0x969F8A3667987823B84C4F22A4CdfEA3Ae724E86', level: 'Senior', reputation: 324, payRate: '$24/hr' },
+  { name: 'John Smith', accountAddress: '0x969F8A3667987823B84C4F22A4CdfEA3Ae724C86', level: 'Senior', reputation: 324, payRate: '$24/hr' },
+  { name: 'John Smith', accountAddress: '0x969F8A3667987823B84C4F22A4CdfEA3Ae724D86', level: 'Senior', reputation: 324, payRate: '$24/hr' },
+  { name: 'John Smith', accountAddress: '0x969F8A3667987823B84C4F22A4CdfEA3Ae724F86', level: 'Senior', reputation: 324, payRate: '$24/hr' },
+  { name: 'John Smith', accountAddress: '0x969F8A3667987823B84C4F22A4CdfEA3Ae724G86', level: 'Senior', reputation: 324, payRate: '$24/hr' },
+  { name: 'John Smith', accountAddress: '0x969F8A3667987823B84C4F22A4CdfEA3Ae724T86', level: 'Senior', reputation: 324, payRate: '$24/hr' },
 ] 
 
 export default () => {
