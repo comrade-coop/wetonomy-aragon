02a346d0dcff65fa4cfaae5def3eef6773ae1c98
Ivan-Asen Chakarov <vankata9907@gmail.com>
2018-07-26 14:17:44 +0300

    TimeTracking: Added limit to hours tracked for a period of time

---
+++ apps/timetracking/test/timetracking.js
@@ -0,0 +1,65 @@
+const TimeTracking = artifacts.require('TimeTracking')
+const { assertThrowsAsync, sleep } = require('../../shared/test-utils/util')
+
+const PERIOD_LENGTH_SECONDS = 5
+const MAX_HOURS = 10
+
+contract('TimeTracking', async (accounts) => {
+
+  it('should initialize the app with the right period length, and limit', async () => {
+    const instance = await TimeTracking.deployed()
+    const owner = accounts[0]
+
+    await instance.initialize(
+      PERIOD_LENGTH_SECONDS, 
+      MAX_HOURS, 
+      { from: owner })
+
+    const periodLengthContract = await instance.periodLength.call()
+    const maxHoursContract = await instance.maxHoursPerPeriod.call()
+
+    assert.equal(PERIOD_LENGTH_SECONDS, periodLengthContract)
+    assert.equal(MAX_HOURS, maxHoursContract)
+  })
+
+  it('should add first tracked hours', async () => {
+    const instance = await TimeTracking.deployed()
+    const owner = accounts[0]
+
+    await instance.trackWork(3, { from:owner })
+    const trackedHours = await instance.addressToTrackedHours.call(owner)
+
+    assert.equal(3, trackedHours)
+  })
+
+  it('should sum up tracked hours', async () => {
+    const instance = await TimeTracking.deployed()
+    const owner = accounts[1]
+
+    await instance.trackWork(1, { from:owner })
+    await instance.trackWork(3, { from:owner })
+
+    const trackedHours = await instance.addressToTrackedHours.call(owner)
+
+    assert.equal(4, trackedHours.toNumber())
+  })
+
+  it('shouldn\'t track hours exceeding limit', async () => {
+    const instance = await TimeTracking.deployed()
+    const owner = accounts[1]
+
+    assertThrowsAsync(instance.trackWork(134, { from:owner }))
+  })
+
+  it('should create a new period after the last one has passed', async () => {
+    const instance = await TimeTracking.deployed()
+    const owner = accounts[1]
+
+    // Multiplying with 1.5 since blocktime doesn't equal real time
+    await sleep(PERIOD_LENGTH_SECONDS * 1000 * 1.5)
+    await instance.trackWork(3, { from:owner })
+
+    const periodCount = await instance.getPeriodsCountForAddress(owner)
+    assert.equal(periodCount.toNumber(), 2)
+  })
+})
