469e0fab34cf318d68b1344450355fd4a5a79527
Ivan-Asen Chakarov <vankata9907@gmail.com>
2018-08-16 13:24:58 +0300

    Fixed Kit build error when TokenManager is added

--- contracts/WetonomyConstants.sol
+++ contracts/WetonomyConstants.sol
@@ -1,7 +1,9 @@
 pragma solidity 0.4.18;
 
+import "@aragon/os/contracts/apm/APMNamehash.sol";
 
-contract WetonomyConstants {
+
+contract WetonomyConstants is APMNamehash {
     uint constant public PCT = 10 ** 16;
     address constant public ANY_ENTITY = address(-1);
 
@@ -9,4 +11,9 @@ contract WetonomyConstants {
     uint constant public DEFAULT_INFLATION_MULTIPLIER = 100;
     uint constant public DEFAULT_TIME_TRACKING_PERIOD_LENGTH_SECONDS = 30 * 60 * 60 * 24;
     uint constant public DEFAULT_MAX_HOURS_PER_PERIOD = 160;
+
+    bytes32 public tokenManagerId = apmNamehash("token-rewards-manager");
+    bytes32 public membersId = apmNamehash("members");
+    bytes32 public timetrackingId = apmNamehash("timetracking");
+    bytes32 public votingId = apmNamehash("voting");
 }
